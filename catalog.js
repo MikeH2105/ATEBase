51a0859bc646caa0a4d8f6528dc8a5a4b487bdcc
Nikolay <97835040+MikeH2105@users.noreply.github.com>
2024-04-03 13:37:21 +0300

    Add files via upload

---
+++ catalog.js
@@ -0,0 +1,18 @@
+const CATALOG = [
+    {
+        id: 'el1',
+        name: 'АВ6620ЕС',
+        polic: '2024.05.01',
+        inspection: '26.01.2024',
+        taxo: '25.06.2024', 
+    },
+
+    {
+        id: 'el2',
+        name: 'АВ5289CI',
+        polic: '2024.05.01',
+        inspection: '26.03.2024',
+        taxo: '25.06.2024', 
+    }
+
+]

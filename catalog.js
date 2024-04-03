06d5fb852d5725b72be908376200b8e7f91a63d7
Nikolay <97835040+MikeH2105@users.noreply.github.com>
2024-04-03 15:42:28 +0300

    Update catalog.js

--- catalog.js
+++ catalog.js
@@ -2,17 +2,32 @@ const CATALOG = [
     {
         id: 'el1',
         name: 'АВ6620ЕС',
-        polic: '2024.05.01',
-        inspection: '26.01.2024',
-        taxo: '25.06.2024', 
+        polic: '2024.01.01',
+        inspection: '2024.04.01',
+        taxo: '2024.01.01', 
     },
 
     {
         id: 'el2',
         name: 'АВ5289CI',
-        polic: '2024.05.01',
-        inspection: '26.03.2024',
+        polic: '2024.09.01',
+        inspection: '2024.04.01',
         taxo: '25.06.2024', 
-    }
+    },
+    {
+        id: 'el3',
+        name: 'АВ4892ЕС',
+        polic: '2024.04.02',
+        inspection: '26.01.2024',
+        taxo: '25.06.2024', 
+    },
+
+    {
+        id: 'el4',
+        name: 'АВ6620ЕС',
+        polic: '2024.04.27',
+        inspection: '26.01.2024',
+        taxo: '2024.03.29', 
+    },
 
 ]
